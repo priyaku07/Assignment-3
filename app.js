b9d8b1602b88508e8151798c0f0333003de04777
priyaku07 <118508844+priyaku07@users.noreply.github.com>
2023-07-28 22:00:53 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
-let count = 0;
-function updateCounter() {
-    count++;
-    document.getElementById('count').textContent = count;
-        }
-        document.getElementById('counterButton').onclick = updateCounter;
+function getRandomColor() {
+    const red = Math.floor(Math.random() * 256);
+    const green = Math.floor(Math.random() * 256);
+    const blue = Math.floor(Math.random() * 256);
+    return `rgb(${red}, ${green}, ${blue})`;
+}
+function changeBackgroundColor() {
+    const randomColor = getRandomColor();
+    document.body.style.backgroundColor = randomColor;
+}
+document.onclick = changeBackgroundColor;
